abe253ac5e48802a6981f127ed4fbdcd7390991e
Leandro P S Carmo <leandrocarmo2012@gmail.com>
2023-10-30 11:14:51 -0300

    feature/profile-page

--- src/components/Profile/styles.ts
+++ src/components/Profile/styles.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import { css } from 'styled-components';
 
 export const ProfilePage = styled.div`
-
+  position: relative;
 `;
 
 export const ProfileHeader = styled.div`
@@ -11,17 +11,25 @@ export const ProfileHeader = styled.div`
   flex-direction: column;
   color: ${props => props.theme.color.white};
   padding: 16px;
+  background-image: url('/assets/icons/pokeball.svg');
+  background-repeat: no-repeat;
+  background-position: 180px top;
+  background-size: 208px auto;
+  z-index: 2;
+  position: sticky;
 
   .headerInfo {
     display: flex;
     flex-direction: row;
     align-items: center;
     justify-content: space-between;
-    padding: 0 40px;
+    padding: 0 24px;
+    margin-bottom: 26px;
   }
 
   h1 {
     font-size: ${props => props.theme.fontSize.xxxl};
+    text-transform: capitalize;
   }
 `;
 
@@ -37,4 +45,98 @@ export const PokeImg = styled.div`
 
 export const ProfileContent = styled.div`
   background-color: ${props => props.theme.color.white};
+  margin: 0 7px;
+  border-radius: 15px;
+  padding: 75px 25px;
+  position: absolute;
+  top: 78%;
+  z-index: 1;
+  font-size: ${props => props.theme.fontSize.s};
+
+  .types {
+    height: 20px;
+    display: flex;
+    align-items: baseline;
+
+    span {
+      padding: 2px 8px;
+      margin-right: 10px;
+      border-radius: 10px;
+      text-align: center;
+      line-height: 1.2;
+      color: ${props => props.theme.color.white};
+    }
+  }
+
+  .profileInfo {
+    display: flex;
+    justify-content: space-between;
+    margin: 35px 0;
+
+    div {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+
+      .info {
+        font-size: ${props => props.theme.fontSize.xs};
+        color: ${props => props.theme.color.gray};
+        margin-top: 12px;
+
+      }
+
+      .weight::before,
+      .height::before {
+        content: "";
+        display: inline-block;
+        background-size: cover;
+        margin-right: 8px;
+        height: 16px;
+      }
+
+      .weight::before {
+        width: 16px;
+        background-image: url('/assets/icons/balance.svg');
+      }
+
+      .height::before {
+        width: 8px;
+        background-image: url('/assets/icons/ruler.svg');
+      }
+    }
+
+  }
+  .baseStats {
+    margin-top: 35px;
+
+    h4 {
+      font-weight: ${props => props.theme.fontWeight.bold};
+      font-size:${props => props.theme.fontSize.l};
+      margin-bottom: 25px;
+    }
+
+    ul {
+      list-style: none;
+
+      li {
+        display: flex;
+        align-items: center;
+
+        .stats {
+          width: 54px;
+          height: 26px;
+          display: flex;
+          align-items: center;
+          margin-right: 14px;
+          border-right: 1px solid ${props => props.theme.color.lightGray};
+          font-size:${props => props.theme.fontSize.s};
+        }
+
+        .power {
+          width: 45px;
+          font-size:${props => props.theme.fontSize.xs};
+        }
+      }
+    }
+  }
 `;
--- src/pages/profile/[id].tsx
+++ src/pages/profile/[id].tsx
@@ -4,114 +4,170 @@ import { Pokemon } from '@/services/types';
 import { usePokemons } from '@/services/pokemonContext';
 import { ProfileContent, ProfileHeader, ProfilePage, PokeImg } from '../../components/Profile/styles';
 import { PokemonTypeColor } from '../../styles/pokemonTypeColor';
+import ProgressBar from '../../components/PokemonCard/progressBar';
 
 const Profile: React.FC = () => {
-    // Acessa o contexto dos pokémons e obtém a lista deles.
-    const { pokemons } = usePokemons();
+  // Acessa o contexto dos pokémons e obtém a lista deles.
+  const { pokemons } = usePokemons();
 
-    // Armazena os detalhes do Pokémon selecionado.
-    const [pokemon, setPokemon] = useState<Pokemon | null>(null);
+  // Armazena os detalhes do Pokémon selecionado.
+  const [pokemon, setPokemon] = useState<Pokemon | null>(null);
 
-    // Acessa os parâmetros da rota.
-    const router = useRouter();
-    const { id } = router.query;
+  // Acessa os parâmetros da rota.
+  const router = useRouter();
+  const { id } = router.query;
 
-    // Carrega o pokemon selecionado.
-    // Caso não encontre redireciona para 404
-    useEffect(() => {
-      const existingPokemon = pokemons.find(p => p.id === Number(id));
+  // Carrega o pokemon selecionado.
+  // Caso não encontre redireciona para 404
+  useEffect(() => {
+    const existingPokemon = pokemons.find(p => p.id === Number(id));
 
-      if (existingPokemon) {
-        setPokemon(existingPokemon);
-      } else {
-        router.push('/404');
+    if (existingPokemon) {
+      setPokemon(existingPokemon);
+    } else {
+      router.push('/404');
+    }
+  }, [id, pokemons]);
+
+  // Atualiza a página de acordo com o tipo do pokemon
+  useEffect(() => {
+    if (pokemon && pokemon.types) {
+      const mainType = pokemon.types[0].type.name;
+      const color = PokemonTypeColor[mainType];
+      if (color) {
+        document.body.style.backgroundColor = color;
       }
-    }, [id, pokemons]);
+    }
 
-    // Atualiza a página de acordo com o tipo do pokemon
-    useEffect(() => {
-      if (pokemon && pokemon.types) {
-        const mainType = pokemon.types[0].type.name;
-        const color = PokemonTypeColor[mainType];
-        if (color) {
-          document.body.style.backgroundColor = color;
-        }
-      }
-
-      return () => {
-        document.body.style.backgroundColor = '#F7F7F7';
-      };
-    }, [pokemon]);
-
-    if (!pokemon) return <p>Carregando...</p>;
-
-    // Obtem os valores das estatísticas do Pokémon.
-    const getStatValue = (statName: string) => {
-      const stat = pokemon.stats.find(s => s.stat.name === statName);
-      return stat ? stat.base_stat : 0;
+    return () => {
+      document.body.style.backgroundColor = '#F7F7F7';
     };
+  }, [pokemon]);
 
-    return (
-      <ProfilePage>
-        <ProfileHeader>
-          <div className="headerInfo">
-            <h1>{pokemon.name}</h1>
-            <span>#{pokemon.id}</span>
+  if (!pokemon) return <p>Carregando...</p>;
+
+  // Obtem os valores das estatísticas do Pokémon.
+  const getStatValue = (statName: string) => {
+    const stat = pokemon.stats.find(s => s.stat.name === statName);
+    return stat ? stat.base_stat : 0;
+  };
+
+  // Retorna os dois primeiros movimentos do Pokémon.
+  const getTopTwoMoves = (): string => {
+    if (!pokemon || !pokemon.moves || pokemon.moves.length < 2) {
+      return "Unknown Moves";
+    }
+    return `${pokemon.moves[0].move.name} / ${pokemon.moves[1].move.name}`;
+  };
+
+  return (
+    <ProfilePage>
+      <ProfileHeader>
+        <div className="headerInfo">
+          <h1>{pokemon.name}</h1>
+          <span>#{pokemon.id}</span>
+        </div>
+        <PokeImg>
+          <img src={pokemon.sprites.other['official-artwork'].front_default} alt={pokemon.name} />
+        </PokeImg>
+      </ProfileHeader>
+      <ProfileContent>
+        <div className="types">
+          {pokemon.types.map(t => (
+            <span key={t.type.name} style={{ backgroundColor: PokemonTypeColor[t.type.name] }}>
+              {t.type.name}
+            </span>
+          ))}
+        </div>
+        <div className='profileInfo'>
+          <div>
+            <span className='weight'>
+              {pokemon.weight / 10} kg
+            </span>
+            <span className='info'>
+              Weight
+            </span>
           </div>
-          <PokeImg>
-            <img src={pokemon.sprites.other['official-artwork'].front_default} alt={pokemon.name} />
-          </PokeImg>
-        </ProfileHeader>
-        <ProfileContent>
-        <div>
-          <span>
-            {pokemon.types.map(t => t.type.name).join(", ")}
-          </span>
+          <div>
+            <span className='height'>
+              {pokemon.height / 10} m
+            </span>
+            <span className='info'>
+              Height
+            </span>
+          </div>
+          <div>
+            <span>
+              {getTopTwoMoves()}
+            </span>
+            <span className='info'>
+              Moves
+            </span>
+          </div>
         </div>
         <div>
           <span>
-            Peso: {pokemon.weight / 10} kg
+            {pokemon.description}
           </span>
         </div>
-        <div>
-          <span>
-            Altura: {pokemon.height / 10} m
-          </span>
-        </div>
-        <div>
-          <span>
-            Descrição: {pokemon.description}
-          </span>
-        </div>
-        <div>
-          <span>
-            HP: {getStatValue('hp')}
-          </span>
-        </div>
-        <div>
-          <span>
-            ATK: {getStatValue('attack')}
-          </span>
-        </div>
-        <div>
-          <span>
-            DEF: {getStatValue('defense')}
-          </span>
-        </div>
-        <div>
-          <span>
-            SATK: {getStatValue('special-attack')}
-          </span>
-        </div>
-        <div>
-          <span>
-            SDEF: {getStatValue('special-defense')}
-          </span>
-        </div>
-        <div>
-          <span>
-            SPD: {getStatValue('speed')}
-          </span>
+        <div className='baseStats'>
+          <h4 style={{ color: PokemonTypeColor[pokemon.types[0].type.name] }}>Base Stats</h4>
+          <ul>
+            <li>
+              <span className='stats' style={{ color: PokemonTypeColor[pokemon.types[0].type.name] }}>HP</span>
+              <span className='power'>{getStatValue('hp')}</span>
+                <ProgressBar
+                  percentage={getStatValue('hp')}
+                  fillColor={PokemonTypeColor[pokemon.types[0].type.name]}
+                  bgColor={PokemonTypeColor[pokemon.types[0].type.name]}
+                />
+            </li>
+            <li>
+              <span className='stats' style={{ color: PokemonTypeColor[pokemon.types[0].type.name] }}>ATK</span>
+              <span className='power'>{getStatValue('attack')}</span>
+              <ProgressBar
+                percentage={getStatValue('attack')}
+                fillColor={PokemonTypeColor[pokemon.types[0].type.name]}
+                bgColor={PokemonTypeColor[pokemon.types[0].type.name]}
+              />
+            </li>
+            <li>
+              <span className='stats' style={{ color: PokemonTypeColor[pokemon.types[0].type.name] }}>DEF</span>
+              <span className='power'>{getStatValue('defense')}</span>
+              <ProgressBar
+                percentage={getStatValue('defense')}
+                fillColor={PokemonTypeColor[pokemon.types[0].type.name]}
+                bgColor={PokemonTypeColor[pokemon.types[0].type.name]}
+              />
+            </li>
+            <li>
+              <span className='stats' style={{ color: PokemonTypeColor[pokemon.types[0].type.name] }}>SATK</span>
+              <span className='power'>{getStatValue('special-attack')}</span>
+              <ProgressBar
+                percentage={getStatValue('special-attack')}
+                fillColor={PokemonTypeColor[pokemon.types[0].type.name]}
+                bgColor={PokemonTypeColor[pokemon.types[0].type.name]}
+              />
+            </li>
+            <li>
+              <span className='stats' style={{ color: PokemonTypeColor[pokemon.types[0].type.name] }}>SDEF</span>
+              <span className='power'>{getStatValue('special-defense')}</span>
+              <ProgressBar
+                percentage={getStatValue('special-defense')}
+                fillColor={PokemonTypeColor[pokemon.types[0].type.name]}
+                bgColor={PokemonTypeColor[pokemon.types[0].type.name]}
+              />
+            </li>
+            <li>
+              <span className='stats' style={{ color: PokemonTypeColor[pokemon.types[0].type.name] }}>SPD</span>
+              <span className='power'>{getStatValue('speed')}</span>
+              <ProgressBar
+                percentage={getStatValue('speed')}
+                fillColor={PokemonTypeColor[pokemon.types[0].type.name]}
+                bgColor={PokemonTypeColor[pokemon.types[0].type.name]}
+              />
+            </li>
+          </ul>
         </div>
       </ProfileContent>
     </ProfilePage>
