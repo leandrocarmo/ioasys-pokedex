9aa43031bef3afbee64044f057c88de2ecf83dba
Leandro P S Carmo <leandrocarmo2012@gmail.com>
2023-11-04 13:07:18 -0300

    feature/infinite-scroll

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,44 +1,39 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Layout from '../../components/Layout/layout';
 import { ContentWrapper } from '@/styles/global';
 import PokemonCard from '@/components/PokemonCard/pokemonCard';
 import { ContentGrid } from '@/components/PokemonCard/style';
 import { Header } from '@/components/Header/Header';
-import { fetchPokemons } from '@/services/pokeAPI';
-import { usePokemons } from '@/context/pokemonContext';
 import Link from 'next/link';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { usePaginationLogic } from '@/components/PaginationLogic/paginationLogic';
 
 function HomePage() {
-  const { pokemons, setPokemons } = usePokemons();
-
-  //carregar os pokémons da API.
-  useEffect(() => {
-    const loadPokemons = async () => {
-      const fetchedPokemons = await fetchPokemons();
-      setPokemons(fetchedPokemons);
-    };
-
-    if (pokemons.length === 0) {
-      loadPokemons();
-    }
-  }, []);
+  const { pokemons, hasMore, loadMorePokemons } = usePaginationLogic(20, 10);
 
   return (
     <Layout>
       <Header />
       <ContentWrapper>
-        <ContentGrid>
-          {pokemons.map((pokemon) => (
-            <Link key={pokemon.id} href={`/profile/${pokemon.id}`} passHref>
-              <PokemonCard
-                id={pokemon.id}
-                types={pokemon.types}
-                name={pokemon.name}
-                image={pokemon.sprites.other['official-artwork'].front_default}
-              />
-            </Link>
-          ))}
-        </ContentGrid>
+        <InfiniteScroll
+          dataLength={pokemons.length}
+          next={loadMorePokemons}
+          hasMore={hasMore}
+          loader={<h4>Loading...</h4>}
+        >
+          <ContentGrid>
+            {pokemons.map((pokemon) => (
+              <Link key={pokemon.id} href={`/profile/${pokemon.id}`} passHref>
+                <PokemonCard
+                  id={pokemon.id}
+                  types={pokemon.types}
+                  name={pokemon.name}
+                  image={pokemon.sprites.other['official-artwork'].front_default}
+                />
+              </Link>
+            ))}
+          </ContentGrid>
+        </InfiniteScroll>
       </ContentWrapper>
     </Layout>
   );
